81bcc8e844e6b0642faf20c9dc80c24a867e32d5
Oleksandr Deundiak <deundiak@gmail.com>
2019-01-24 16:46:11 +0200

    Fixing deploys

---
+++ CI/publish-cocoapods.sh
@@ -0,0 +1,5 @@
+gem install cocoapods --pre;
+pod trunk push VirgilCryptoCommon.podspec;
+pod trunk push VirgilCryptoFoundation.podspec;
+pod trunk push VirgilCryptoPythia.podspec;
+pod trunk push VirgilCryptoRatchet.podspec;
