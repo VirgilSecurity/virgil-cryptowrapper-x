b9d0336a1401ee6c2264042308c1857401f77b33
Oleksandr Deundiak <deundiak@gmail.com>
2019-01-24 14:54:14 +0200

    Added docs generation

---
+++ CI/generate-docs.sh
@@ -0,0 +1,12 @@
+gem install jazzy
+
+SCHEME="${PROJ}_macOS"
+
+jazzy \
+--author "Virgil Security" \
+--author_url "https://virgilsecurity.com/" \
+--xcodebuild-arguments -scheme,"${SCHEME}" \
+--module "${PROJ}" \
+--output "${OUTPUT}" \
+--hide-documentation-coverage \
+--theme apple
