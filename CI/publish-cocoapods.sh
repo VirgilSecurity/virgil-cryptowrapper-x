4656a602f68c569bb13a96a3ff39d306c02a7730
Oleksandr Deundiak <deundiak@gmail.com>
2019-11-04 14:11:17 +0200

    Fixing cocoapods release

--- CI/publish-cocoapods.sh
+++ CI/publish-cocoapods.sh
@@ -1,6 +1,4 @@
-sudo gem install cocoapods --pre;
-pod repo update;
 pod trunk push VirgilCryptoFoundation.podspec;
-pod repo update;
+pod repo update master;
 pod trunk push VirgilCryptoPythia.podspec;
 pod trunk push VirgilCryptoRatchet.podspec;
