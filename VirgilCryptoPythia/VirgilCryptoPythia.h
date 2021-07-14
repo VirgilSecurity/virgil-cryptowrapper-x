bf4f0d561b84f5968367d116282e3ee287448677
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-14 21:33:55 +0300

    Updated licence period

--- VirgilCryptoPythia/VirgilCryptoPythia.h
+++ VirgilCryptoPythia/VirgilCryptoPythia.h
@@ -1,6 +1,6 @@
 //  @license
 // --------------------------------------------------------------------------
-//  Copyright (C) 2015-2019 Virgil Security, Inc.
+//  Copyright (C) 2015-2021 Virgil Security, Inc.
 //
 //  All rights reserved.
 //
