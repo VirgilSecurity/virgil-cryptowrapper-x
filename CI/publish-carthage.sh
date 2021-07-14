6039dc7748139fae16e30b286732f078add69203
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-14 20:57:21 +0300

    Added TODO

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -4,6 +4,7 @@ brew update;
 brew outdated carthage || brew upgrade carthage;
 carthage build --use-xcframeworks --no-skip-current;
 
+# TODO: Should be replaced by carthage archive, when it supports xcframeworks
 rm -r ${FRAMEWORKS_PATH}/VSCCommon.xcframework ${FRAMEWORKS_PATH}/VSCFoundation.xcframework ${FRAMEWORKS_PATH}/VSCPythia.xcframework ${FRAMEWORKS_PATH}/VSCRatchet.xcframework Carthage/Checkouts
 
 zip -r VirgilCryptoWrapper.xcframework.zip Carthage
