ed34a77da8f3aeb4eaefe257fc4366ddd1e47ded
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-17 18:53:26 +0300

    Fixed tvOS & watchOS destinations for travis, updated public-carthage.sh

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -1,10 +1,8 @@
-FRAMEWORKS_PATH=Carthage/Build
-
-brew update;
-brew outdated carthage || brew upgrade carthage;
-carthage build --use-xcframeworks --no-skip-current;
+#brew update;
+#brew outdated carthage || brew upgrade carthage;
+#carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
-rm -r ${FRAMEWORKS_PATH}/VSCCommon.xcframework ${FRAMEWORKS_PATH}/VSCFoundation.xcframework ${FRAMEWORKS_PATH}/VSCPythia.xcframework ${FRAMEWORKS_PATH}/VSCRatchet.xcframework Carthage/Checkouts
-
-zip -r VirgilCryptoWrapper.xcframework.zip Carthage
+FRAMEWORKS_PATH=Carthage/Build
+find ${FRAMEWORKS_PATH} ! -name 'VirgilCrypto*' -delete
+zip -r VirgilCryptoWrapper.xcframework.zip ${FRAMEWORKS_PATH}
