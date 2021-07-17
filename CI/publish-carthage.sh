f412045ac856eea630729510244d786e13a5a724
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-17 22:03:44 +0300

    Fixed typo

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -1,6 +1,6 @@
-#brew update;
-#brew outdated carthage || brew upgrade carthage;
-#carthage build --use-xcframeworks --no-skip-current;
+brew update;
+brew outdated carthage || brew upgrade carthage;
+carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
 FRAMEWORKS_PATH=Carthage/Build
