5087f4b5d5286646df6b7900c4aa82c70be6b98b
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-19 23:16:00 +0300

    Updated publish-cocoapods.sh script

--- CI/publish-cocoapods.sh
+++ CI/publish-cocoapods.sh
@@ -1,4 +1,9 @@
 pod trunk push VirgilCryptoFoundation.podspec;
+
+rm -fr ~/.cocoapods/repos;
+rm -fr ~/Library/Caches/CocoaPods/;
+pod setup;
 pod repo update;
+
 pod trunk push VirgilCryptoPythia.podspec;
 pod trunk push VirgilCryptoRatchet.podspec;
