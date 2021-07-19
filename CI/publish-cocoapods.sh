fc037dcaadca6de3de422c2963cba9d63e3b08eb
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-07-20 01:01:46 +0300

    Removed publishing podspecs of Pythia & Ratchet on CI

--- CI/publish-cocoapods.sh
+++ CI/publish-cocoapods.sh
@@ -1,9 +1,1 @@
 pod trunk push VirgilCryptoFoundation.podspec;
-
-rm -fr ~/.cocoapods/repos;
-rm -fr ~/Library/Caches/CocoaPods/;
-pod setup;
-pod repo update;
-
-pod trunk push VirgilCryptoPythia.podspec;
-pod trunk push VirgilCryptoRatchet.podspec;
