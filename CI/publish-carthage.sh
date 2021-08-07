7a5f5e6d24f3c7bb54c19939218a0f8b3147ced6
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-07 15:55:57 +0300

    Fixed publish-carthage.sh #2

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -4,5 +4,5 @@ carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
 FRAMEWORKS_PATH=Carthage/Build
-find ${FRAMEWORKS_PATH} ! -name 'VirgilCrypto*' -maxdepth 1 -delete
+find ${FRAMEWORKS_PATH} -mindepth 1 -maxdepth 1 ! -name 'VirgilCrypto*' -delete
 zip -r VirgilCryptoWrapper.xcframework.zip ${FRAMEWORKS_PATH}
