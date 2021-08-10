a65e9400fd6e0946f2145a280e5c2f372081c14c
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-10 22:42:33 +0300

    Fixed CI deploy

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -6,3 +6,4 @@ carthage build --use-xcframeworks --no-skip-current;
 FRAMEWORKS_PATH=Carthage/Build
 find ${FRAMEWORKS_PATH} -mindepth 1 -maxdepth 1 ! -name 'VirgilCrypto*' -exec rm -rvf {} \;
 zip -r VirgilCryptoWrapper.xcframework.zip ${FRAMEWORKS_PATH}
+carthage update --use-xcframeworks;
