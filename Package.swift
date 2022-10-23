dbc77649bbc166c4a3b5df956e1bdcee67b8d1f0
SergeySeroshtan <sergey.seroshtan@gmail.com>
2022-10-24 01:02:34 +0300

    Update deployment versions for iOS, tvOS, watchOS (spm)   - iOS 9.0 -> 11.0   - tvOS 9.0 -> 11.0   - watchOS 2.0 -> 4.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "VirgilCryptoWrapper",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_10), .iOS(.v11), .tvOS(.v11), .watchOS(.v4)
     ],
     products: [
         .library(
